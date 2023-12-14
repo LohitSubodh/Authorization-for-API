f4db86a9baae55c0a7d838d988459b94acdfe26b
LohitSubodh <lohi.s.ranger@gmail.com>
2023-12-14 13:54:58 +0530

    first commit

---
+++ src/main/java/com/example/API/Authorizarion/ApiAuthorizarionApplication.java
@@ -0,0 +1,13 @@
+package com.example.API.Authorizarion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiAuthorizarionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiAuthorizarionApplication.class, args);
+	}
+
+}
